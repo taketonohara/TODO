8dce003e097a55b216b75ade2ae24cb5ff7dd4e4
taketo <takenoha@icloud.com>
2020-07-04 21:09:36 +0900

    ToDoタスク

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  
+has_many :lists, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: { maximum: 20 }
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'top#index'
+
+  resources :list, only: %i(new create edit update destroy) do
+   resources :card, only: %i(new create show edit update)
+  end
+end
